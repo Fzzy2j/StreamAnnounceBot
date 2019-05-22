8424b8e086ed165d2e18dcfa06829b59e2d7ecd4
Fzzy <fzzysoda@gmail.com>
2019-05-21 20:17:15 -0700

    is actually possible to setup now

--- src/main/kotlin/me/fzzy/announcebot/Main.kt
+++ src/main/kotlin/me/fzzy/announcebot/Main.kt
@@ -3,6 +3,7 @@ package me.fzzy.announcebot
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.google.gson.stream.JsonReader
+import org.json.JSONObject
 import sx.blah.discord.Discord4J
 import sx.blah.discord.api.ClientBuilder
 import sx.blah.discord.api.IDiscordClient
@@ -13,29 +14,57 @@ import java.io.File
 import java.io.FileWriter
 import java.io.InputStreamReader
 
-lateinit var twitchToken: String
 lateinit var cli: IDiscordClient
-
-var broadcastChannelId = 288805361341693952
 
 val gson = Gson()
 
 val streamsFile = File("streams.json")
 
-fun main(args: Array<String>) {
-    val discordToken = args[0]
-    twitchToken = args[1]
+val speedRunTagId = "7cefbf30-4c3e-4aa7-99cd-70aabb662f27"
 
-    cli = ClientBuilder().withToken(discordToken).build()
+class Config {
+    val broadcastChannelId = 0L
+    val game = ""
+    val discordToken = ""
+    val twitchToken = ""
+}
+
+lateinit var config: Config
+
+fun main() {
+
+    fun configReq() {
+        Discord4J.LOGGER.error("====================================================================================================================================")
+        Discord4J.LOGGER.error("For this bot to work you must set values in config.json, instructions can be found here: https://github.com/Fzzy2j/StreamAnnounceBot")
+        Discord4J.LOGGER.error("====================================================================================================================================")
+        System.exit(0)
+    }
+
+    val configFile = File("config.json")
+    if (!configFile.exists()) {
+        val bufferWriter = BufferedWriter(FileWriter(configFile.absoluteFile, false))
+        val save = JSONObject(gson.toJson(Config()))
+        bufferWriter.write(save.toString(2))
+        bufferWriter.close()
+
+        configReq()
+    }
+
+    try {
+        config = gson.fromJson(JsonReader(InputStreamReader(configFile.inputStream())), Config::class.java)
+        if (config.broadcastChannelId == 0L || config.game.isBlank() || config.discordToken.isBlank() || config.twitchToken.isBlank())
+            configReq()
+    } catch(e: Exception) {
+        Discord4J.LOGGER.error("Could not load json from config file. Did you edit it improperly?")
+        e.printStackTrace()
+        System.exit(0)
+    }
+
+    cli = ClientBuilder().withToken(config.discordToken).build()
     cli.dispatcher.registerListener(ReadyListener)
 
     Discord4J.LOGGER.info("Logging in.")
     cli.login()
-    val token = object : TypeToken<List<Stream>>() {}
-    if (streamsFile.exists())
-        StreamScanner.activeStreams =
-            gson.fromJson(JsonReader(InputStreamReader(streamsFile.inputStream())), token.type)
-    StreamScanner.start()
 }
 
 fun saveStreams(streams: ArrayList<Stream>) {
@@ -52,5 +81,13 @@ object ReadyListener {
         for (guild in event.client.guilds) {
             Discord4J.LOGGER.info(guild.name)
         }
+
+        Discord4J.LOGGER.info("Starting Scanner...")
+
+        val token = object : TypeToken<List<Stream>>() {}
+        if (streamsFile.exists())
+            StreamScanner.activeStreams =
+                gson.fromJson(JsonReader(InputStreamReader(streamsFile.inputStream())), token.type)
+        StreamScanner.start()
     }
 }
--- src/main/kotlin/me/fzzy/announcebot/StreamScanner.kt
+++ src/main/kotlin/me/fzzy/announcebot/StreamScanner.kt
@@ -6,31 +6,46 @@ import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
 import org.json.JSONObject
 import sx.blah.discord.Discord4J
+import sx.blah.discord.handle.obj.IChannel
 import sx.blah.discord.util.RequestBuffer
 
 object StreamScanner : Thread() {
 
     var activeStreams = arrayListOf<Stream>()
-
-    private val speedRunTagId = "7cefbf30-4c3e-4aa7-99cd-70aabb662f27"
-    private val tf2Id = "489201"
+    var broadcastChannel: IChannel? = null
 
     override fun run() {
+        broadcastChannel = cli.getChannelByID(config.broadcastChannelId)
+        if (broadcastChannel == null) {
+            Discord4J.LOGGER.error("Could not retrieve broadcast channel, did you enter the correct id in the config?")
+            System.exit(0)
+            return
+        }
+        val gameId = try {
+            getGameIdRequest(config.game)
+        } catch(e: Exception) {
+            Discord4J.LOGGER.error("Could not retrieve game id from twitch, is the game name exactly as it is on the twitch directory?")
+            e.printStackTrace()
+            System.exit(0)
+            return
+        }
+        Discord4J.LOGGER.info("Game id found: $gameId")
+        Discord4J.LOGGER.info("Scanning has commenced.")
         while (true) {
             try {
-                Thread.sleep(60 * 1000)
-
-                var json = getTitanfallRequest()
+                var json = getStreamsRequest(gameId)
                 var array = json.getJSONArray("data")
 
                 // Storing all the pages of streams
                 val requestStreams = hashMapOf<Long, Stream>()
 
+
                 for (i in 0 until array.length())
                     requestStreams[array.getJSONObject(i).getLong("user_id")] = Stream(array.getJSONObject(i))
 
                 while (json.getJSONObject("pagination").has("cursor")) {
-                    json = getTitanfallRequest(json.getJSONObject("pagination").getString("cursor"))
+                    sleep(3000)
+                    json = getStreamsRequest(gameId, json.getJSONObject("pagination").getString("cursor"))
                     array = json.getJSONArray("data")
                     for (i in 0 until array.length())
                         requestStreams[array.getJSONObject(i).getLong("user_id")] = Stream(array.getJSONObject(i))
@@ -54,13 +69,15 @@ object StreamScanner : Thread() {
                         activeStreams.add(requestStream)
 
                         val msg = "${requestStream.title} https://www.twitch.tv/${requestStream.username}"
-                        RequestBuffer.request { cli.getChannelByID(broadcastChannelId).sendMessage(msg) }
+                        RequestBuffer.request { broadcastChannel?.sendMessage(msg) }
                         Discord4J.LOGGER.info("${requestStream.username} is now live.")
                     }
                 }
 
                 // Save the active streams
                 saveStreams(activeStreams)
+
+                sleep(60 * 1000)
             } catch (e: Exception) {
                 Discord4J.LOGGER.info("Error while trying to scan streams:")
                 e.printStackTrace()
@@ -68,12 +85,22 @@ object StreamScanner : Thread() {
         }
     }
 
-    fun getTitanfallRequest(pagination: String? = null): JSONObject {
-        val uriBuilder = URIBuilder("https://api.twitch.tv/helix/streams").addParameter("game_id", tf2Id)
+    fun getGameIdRequest(name: String): Int {
+        val uriBuilder = URIBuilder("https://api.twitch.tv/helix/games").addParameter("name", name)
+        val uri = uriBuilder.build()
+        val http = HttpGet(uri)
+        http.addHeader("Client-ID", config.twitchToken)
+        val response = HttpClients.createDefault().execute(http)
+        val json = JSONObject(EntityUtils.toString(response.entity))
+        return json.getJSONArray("data").getJSONObject(0).getInt("id")
+    }
+
+    fun getStreamsRequest(gameId: Int, pagination: String? = null): JSONObject {
+        val uriBuilder = URIBuilder("https://api.twitch.tv/helix/streams").addParameter("game_id", gameId.toString())
         if (pagination != null) uriBuilder.addParameter("after", pagination)
         val uri = uriBuilder.build()
         val http = HttpGet(uri)
-        http.addHeader("Client-ID", twitchToken)
+        http.addHeader("Client-ID", config.twitchToken)
         val response = HttpClients.createDefault().execute(http)
         return JSONObject(EntityUtils.toString(response.entity))
     }
@@ -85,7 +112,7 @@ object StreamScanner : Thread() {
         )
         val uri = uriBuilder.build()
         val http = HttpGet(uri)
-        http.addHeader("Client-ID", twitchToken)
+        http.addHeader("Client-ID", config.twitchToken)
         val response = HttpClients.createDefault().execute(http)
         return JSONObject(EntityUtils.toString(response.entity))
     }
