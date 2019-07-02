46c5b889e8aa2a30ecbcaefd47f87307d90dac82
Fzzy <fzzysoda@gmail.com>
2019-07-02 11:15:01 -0700

    new discord4j fixes

--- src/main/kotlin/me/fzzy/announcebot/Main.kt
+++ src/main/kotlin/me/fzzy/announcebot/Main.kt
@@ -5,14 +5,26 @@ import com.google.gson.reflect.TypeToken
 import com.google.gson.stream.JsonReader
 import discord4j.core.DiscordClient
 import discord4j.core.DiscordClientBuilder
+import discord4j.core.`object`.entity.Channel
+import discord4j.core.`object`.entity.TextChannel
+import discord4j.core.`object`.util.Snowflake
 import discord4j.core.event.domain.lifecycle.ReadyEvent
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.utils.URIBuilder
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.util.EntityUtils
+import org.json.JSONArray
 import org.json.JSONObject
+import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import reactor.util.Logger
 import reactor.util.Loggers
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
 import java.io.InputStreamReader
+import java.util.concurrent.TimeUnit
+import kotlin.system.exitProcess
 
 lateinit var cli: DiscordClient
 val log: Logger = Loggers.getLogger("StreamAnnounce")
@@ -32,6 +44,11 @@ class Config {
 
 lateinit var config: Config
 
+var activeStreams = arrayListOf<Stream>()
+var pagination: String? = null
+
+val scheduler = Schedulers.elastic()
+
 fun main() {
 
     fun configReq() {
@@ -39,7 +56,7 @@ fun main() {
         log.error("===================================")
         log.error("For this bot to work you must set values in config.json, instructions can be found here: https://github.com/Fzzy2j/StreamAnnounceBot")
         log.error("===================================")
-        System.exit(0)
+        exitProcess(0)
     }
 
     val configFile = File("config.json")
@@ -59,7 +76,7 @@ fun main() {
     } catch (e: Exception) {
         log.error("Could not load json from config file. Did you edit it improperly?")
         e.printStackTrace()
-        System.exit(0)
+        exitProcess(0)
     }
 
     cli = DiscordClientBuilder(config.discordToken).build()
@@ -68,9 +85,73 @@ fun main() {
 
     val token = object : TypeToken<List<Stream>>() {}
     if (streamsFile.exists())
-        StreamScanner.activeStreams =
+        activeStreams =
             gson.fromJson(JsonReader(InputStreamReader(streamsFile.inputStream())), token.type)
-    StreamScanner.start()
+
+    val gameId = try {
+        getGameIdRequest(config.game)
+    } catch (e: Exception) {
+        log.error("Could not retrieve game id from twitch, is the game name exactly as it is on the twitch directory?")
+        e.printStackTrace()
+        exitProcess(0)
+    }
+    log.info("Game id found: $gameId")
+
+    scheduler.schedulePeriodically({
+        var json: JSONObject? = null
+        try {
+            json = getStreamsRequest(gameId, pagination)
+
+            val array = json.getJSONArray("data")
+            val requestStreams = hashMapOf<Long, Stream>()
+
+            // Storing all the pages of streams
+            for (i in 0 until array.length())
+                requestStreams[array.getJSONObject(i).getLong("user_id")] = Stream(array.getJSONObject(i))
+
+            // Add new streams and broadcast them
+            for (requestStream in requestStreams.values) {
+                if (!activeStreams.contains(requestStream)) {
+                    if (requestStream.tags.size == 0 || !requestStream.tags.contains(speedRunTagId)) continue
+
+                    activeStreams.add(requestStream)
+
+                    val msg = "${requestStream.title} https://www.twitch.tv/${requestStream.username}"
+                    scheduler.schedule {
+                        (cli.getChannelById(Snowflake.of(config.broadcastChannelId)).block()!! as TextChannel).createMessage(msg).block()
+                    }
+                    log.info("${requestStream.username} is now live.")
+                }
+            }
+
+            if (json.getJSONObject("pagination")!!.has("cursor")) {
+                // Scrolling through pages
+                pagination = json.getJSONObject("pagination").getString("cursor")
+            } else {
+                // Last page of requests
+                pagination = null
+
+                // Remove inactive streams
+                val iterator = activeStreams.iterator()
+                while (iterator.hasNext()) {
+                    val entry = iterator.next()
+                    if (!requestStreams.containsKey(entry.userId) || !entry.tags.contains(speedRunTagId)) {
+                        log.info("${entry.username} is no longer live.")
+                        iterator.remove()
+                    }
+                }
+            }
+
+            // Save the active streams
+            saveStreams(activeStreams)
+        } catch (e: Exception) {
+            if (json != null) {
+                log.error(json.toString(2))
+            }
+            log.error("Error while trying to scan streams:")
+            e.printStackTrace()
+        }
+    }, 10, 60, TimeUnit.SECONDS)
 
     log.info("Logging in.")
     cli.login().block()
@@ -78,7 +159,39 @@ fun main() {
 
 fun saveStreams(streams: ArrayList<Stream>) {
     val bufferWriter = BufferedWriter(FileWriter(streamsFile.absoluteFile, false))
-    val save = gson.toJson(streams)
-    bufferWriter.write(save)
+    val save = JSONArray(gson.toJson(streams))
+    bufferWriter.write(save.toString(2))
     bufferWriter.close()
+}
+
+fun getGameIdRequest(name: String): Int {
+    val uriBuilder = URIBuilder("https://api.twitch.tv/helix/games").addParameter("name", name)
+    val uri = uriBuilder.build()
+    val http = HttpGet(uri)
+    http.addHeader("Client-ID", config.twitchToken)
+    val response = HttpClients.createDefault().execute(http)
+    val json = JSONObject(EntityUtils.toString(response.entity))
+    return json.getJSONArray("data").getJSONObject(0).getInt("id")
+}
+
+fun getStreamsRequest(gameId: Int, pagination: String? = null): JSONObject {
+    val uriBuilder = URIBuilder("https://api.twitch.tv/helix/streams").addParameter("game_id", gameId.toString())
+    if (pagination != null) uriBuilder.addParameter("after", pagination)
+    val uri = uriBuilder.build()
+    val http = HttpGet(uri)
+    http.addHeader("Client-ID", config.twitchToken)
+    val response = HttpClients.createDefault().execute(http)
+    return JSONObject(EntityUtils.toString(response.entity))
+}
+
+fun getTagRequest(broadcasterId: Long): JSONObject {
+    val uriBuilder = URIBuilder("https://api.twitch.tv/helix/streams/tags").addParameter(
+        "broadcaster_id",
+        broadcasterId.toString()
+    )
+    val uri = uriBuilder.build()
+    val http = HttpGet(uri)
+    http.addHeader("Client-ID", config.twitchToken)
+    val response = HttpClients.createDefault().execute(http)
+    return JSONObject(EntityUtils.toString(response.entity))
 }
